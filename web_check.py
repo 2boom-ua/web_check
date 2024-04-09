3e01113d9d4da093fe1cf190ce8be65440765266
Vladimi2boom <97739181+Vladimi2boom@users.noreply.github.com>
2024-04-09 11:42:20 +0300

    Add files via upload

--- web_check.py
+++ web_check.py
@@ -16,6 +16,7 @@ from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 
 def send_message(message : str):
+	message = message.replace("\t", "")
 	if TELEGRAM_ON:
 		try:
 			tb.send_message(CHAT_ID, message, parse_mode="markdown")
@@ -23,10 +24,10 @@ def send_message(message : str):
 			print(f"error: {e}")
 	if DISCORD_ON:
 		try:
-			notifier.send(message.replace("*", "**").replace("\t", ""), print_message=False)
+			notifier.send(message.replace("*", "**"), print_message=False)
 		except Exception as e:
 			print(f"error: {e}")
-	message = message.replace("*", "").replace("\t", "")
+	message = message.replace("*", "")
 	header = message[:message.index("\n")].rstrip("\n")
 	message = message[message.index("\n"):].strip("\n")
 	if GOTIFY_ON:
